d9e587e3996fe88c4ccab54cf0b82c183464a0af
baizhen@efacedata.com <baizhen@efacedata.com>
2018-08-24 09:32:08 +0800

    S

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/': {
-        target: 'http://112.35.60.16:8777',
+        target: 'http://192.168.3.252:8777',
         changeOrigin: true
       }
     },
